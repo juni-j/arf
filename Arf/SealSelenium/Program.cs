429b636f93070b5b236664552b299ff1b43a721c
M <Gr>
2014-08-14 17:04:54 +0200

    seleniumtest added

---
+++ Arf/SealSelenium/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+
+namespace SealSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://www.bing.com");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+            searchInput.SendKeys("cute seals");
+            searchInput.SendKeys(Keys.Enter);
+            driver.Close();
+        }
+    }
+}
+
+
+//namespace Test {
+
+//    [TestFixture()]
+//    public class testEtt {
+
+//        //[SetUp]
+//        //public void Init (){
+//        //IWebDriver driver = new FirefoxDriver();
+//        //}
+
+//        [Test]
+//        public void searchForSeals(){
+//            IWebDriver driver = new FirefoxDriver();
+//            driver.Navigate().GoToUrl("http://www.bing.com");
+//            driver.Manage().Window.Maximize();
+
+//            IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+//            searchInput.SendKeys("cute seals");
+//            searchInput.SendKeys(Keys.Enter);
+//            driver.Close();
+//        }
+
+//        //[TearDown]
+    
+//    }
+
+
+//}
---
+++ Arf/TestSealSelenium/CodedUITest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace TestSealSelenium
+{
+    [TestClass]
+    public class POAPostTest
+    {
+        [TestMethod]
+        public void SearchForCuteSealsBing()
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://www.bing.com");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+            searchInput.SendKeys("cute seals");
+            searchInput.SendKeys(OpenQA.Selenium.Keys.Enter);
+            driver.Close();
+
+        }
+
+        [TestMethod]
+        public void SearchForCuteSealsGoogle()
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchInput = driver.FindElement(By.Id("gbqfq"));
+            searchInput.SendKeys("cute seals");
+            searchInput.SendKeys(OpenQA.Selenium.Keys.Enter);
+            driver.Close();
+
+        }
+    }
+}
